22307ac3897298a4d018b42a599a8c3c3596dd08
OkaGouhei <g.oka1975@gmail.com>
2019-10-03 06:55:00 +0900

    修正
    変数名の頭を小文字

--- RPGgame.java
+++ RPGgame.java
@@ -1,50 +1,50 @@
 import java.util.Random;
 public class RPGgame{
 	public static void main(String args[]) {
-		Character[] TeamA = new Character[4];
-		Character[] TeamB = new Character[4];
-		TeamA[0]= new Character("ルパン",1000);
-		TeamA[1]= new Character("五右衛門",1000);
-		TeamA[2]= new Character("次元",1000);
-		TeamA[3]= new Character("不二子",1500);
-		for(int i=0;i < TeamA.length;i++){
-			TeamA[i].introduce();
+		Character[] teamA = new Character[4];
+		Character[] teamB = new Character[4];
+		teamA[0]= new Character("ルパン",1000);
+		teamA[1]= new Character("五右衛門",1000);
+		teamA[2]= new Character("次元",1000);
+		teamA[3]= new Character("不二子",1500);
+		for(int i=0;i < teamA.length;i++){
+			teamA[i].introduce();
 		}
-		TeamB[0]= new Magician("lucifer",500,40);
-		TeamB[1]= new Magician("マリック",500,30);
-		TeamB[2]= new Monster("ポケモン",500);
-		TeamB[3]= new Monster("ポケポケ",500);
-		for(int i=0;i < TeamB.length;i++){
-			TeamB[i].introduce();
+		teamB[0]= new Magician("lucifer",500,40);
+		teamB[1]= new Magician("マリック",500,30);
+		teamB[2]= new Monster("ポケモン",500);
+		teamB[3]= new Monster("ポケポケ",500);
+		for(int i=0;i < teamB.length;i++){
+			teamB[i].introduce();
 		}
-		int TeamNumber =0;
-		while(shoubu(TeamA)&shoubu(TeamB)){
-			Character[] Team1 ;
-			Character[] Team2 ;
-			if(TeamNumber%2==0){
-				Team1 = TeamA;
-				Team2 = TeamB;
+		int teamNumber =0;
+		while(shoubu(teamA)&shoubu(teamB)){
+			Character[] team1 ;
+			Character[] team2 ;
+			if(teamNumber%2==0){
+				team1 = teamA;
+				team2 = teamB;
 			}else{
-				Team1 = TeamB;
-				Team2 = TeamA;
+				team1 = teamB;
+				team2 = teamA;
 			}
-			TeamNumber++;
+			teamNumber++;
 			Random r = new Random();
 			int number1;
 			int number2;
 			do{
 				number1 = r.nextInt(4);
 				number2 = r.nextInt(4);
-			}while(Team1[number1].getHp()<=0 || Team2[number2].getHp()<=0);
-			Team1[number1].attack(Team2[number2]);
-			if(Team2[number2].hp <= 0){
-				System.out.println(Team2[number2].name +"は生き絶えた");
+			}while(team1[number1].getHp()<=0 || team2[number2].getHp()<=0);
+			team1[number1].attack(team2[number2]);
+			if(team2[number2].hp <= 0){
+				System.out.println(team2[number2].name +"は生き絶えた");
 			}
-			if(!shoubu(Team2)){
-				if(TeamNumber%2==0){
-					System.out.println("TeamBは全滅した");
+			if(!shoubu(team2)){
+				if(teamNumber%2==0){
+					System.out.println("teamBは全滅した");
 				}else {
-					System.out.println("TeamAは全滅した");
+					System.out.println("teamAは全滅した");
 				}
 			}
 		}
